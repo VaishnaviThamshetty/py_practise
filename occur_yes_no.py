ba56b70e1ab4bfb8124642cb90207a78b075b642
VaishnaviThamshetty <39074336+VaishnaviThamshetty@users.noreply.github.com>
2018-05-13 22:25:42 +0530

    Update occur_yes_no.py

--- occur_yes_no.py
+++ occur_yes_no.py
@@ -1,11 +1,12 @@
 a_list=[2,8,3,2,3,1,2,9]
 occur_dict={}
-flaf=0
+flag=0
 
 for i in a_list:
-    flag=1
+    
     x=a_list.count(i)
     if x > 1:
+        flag=1
         occur_dict[i] = x
 if flag==1:
     print(" it has duplicates , the duplicates are")
