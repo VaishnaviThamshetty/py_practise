fa550a7006b666d99193de49d9962d9c1da95e98
VaishnaviThamshetty <39074336+VaishnaviThamshetty@users.noreply.github.com>
2018-05-15 08:34:41 +0530

    Update fun_op.py

--- fun_op.py
+++ fun_op.py
@@ -1,7 +1,7 @@
 num1 = 2
 num2 = 6
 def add(num1,num2):
-    return(num1 + num2)
+    return num1 + num2
 def sub(num1,num2):    
     return(num1 - num2)
 def div(num1,num2):
@@ -10,13 +10,13 @@ def mul(num1,num2):
     return(num1 * num2)
 def mod(num1,num2):
     return(num1 % num2)
-sum1=add(num1,num2)
+sum1 = add(num1,num2)
 print('addition=',sum1)
-sub1=sub(num1,num2)
+sub1 = sub(num1,num2)
 print('subtraction=',sub1)
-mul1=mul(num1,num2)
+mul1 = mul(num1,num2)
 print('multiplication=',mul1)
-div1=div(num1,num2)
+div1 = div(num1,num2)
 print('division=',div1)
-mod1=mod(num1,num2)
+mod1 = mod(num1,num2)
 print('modulus=',mod1)
